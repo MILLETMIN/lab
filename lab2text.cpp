feb233f6b038d1d21d54a671ae819a9de0d0071f
MILLETMIN <90825974+MILLETMIN@users.noreply.github.com>
2021-09-24 00:10:15 +0800

    Add files via upload

---
+++ lab2text.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<sstream>
+#include<string.h>
+#include<vector>
+#include<stack>
+int main(){
+    int i=1;
+    double j=0;
+    long f;
+    switch(i){
+        case 0:
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        default:
+            break;
+    }
+    switch(i){
+        case 0:
+            break;
+        case 1:
+            break;
+        default:
+            break;
+    }
+    if(i<0){
+        if(i<-1){}
+        else{}
+    }
+    else if(i>0){
+        if (i>2){}
+        else if (i==2) {}
+        else if (i>1) {}
+        else {}
+    }
+    else{
+        if(j!=0){}
+        else{}
+    }
+    return 0;
+}
